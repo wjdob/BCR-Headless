71967ee8ad52cfb41b46f0fc044e78ee4418455e
wjdob <w.dob@hotmail.com>
2026-05-16 16:30:00 -0600

    Point release update metadata at main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,7 @@ val gitVersionTriple = describeVersion(git)
 val projectVersionMajor = 1
 val projectVersionMinor = 1
 val projectVersionPatch = 0
-val projectVersionSuffix = "test.3"
+val projectVersionSuffix = "test.4"
 val gitVersionCode = getVersionCode(
     projectVersionMajor,
     projectVersionMinor,
